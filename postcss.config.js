7df5c0d3de058adc5b6340eab0c1f058bfb543e1
sevillalaverde <sevillalaverde@gmail.com>
2025-09-17 14:25:25 +0100

    fix: Convert postcss.config to module.exports format

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-/** @type {import('postcss').Config} */
-const config = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
